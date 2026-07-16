8953652b6fdecb3755a302015b06d71122f9f660
sharkbeans <junyitan2003@gmail.com>
2026-07-16 13:10:50 +0800

    fix

--- docker/postgres/init/01-create-mirror-db.sh
+++ docker/postgres/init/01-create-mirror-db.sh
@@ -2,18 +2,23 @@
 set -eu
 
 MIRROR_DB="${MIRROR_POSTGRES_DB:-indexer_mirror}"
+POSTGRES_ADMIN_DB="${POSTGRES_DB:-postgres}"
 
 if [ -z "$MIRROR_DB" ]; then
   echo "MIRROR_POSTGRES_DB is empty; skipping mirror DB creation"
   exit 0
 fi
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-SQL
-DO \$\$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = '${MIRROR_DB}') THEN
-    EXECUTE format('CREATE DATABASE %I', '${MIRROR_DB}');
-  END IF;
-END
-\$\$;
-SQL
+if psql -v ON_ERROR_STOP=1 \
+  --username "$POSTGRES_USER" \
+  --dbname "$POSTGRES_ADMIN_DB" \
+  --tuples-only \
+  --no-align \
+  --command "SELECT 1 FROM pg_database WHERE datname = '$MIRROR_DB'" \
+  | grep -q '^1$'; then
+  echo "Mirror database '$MIRROR_DB' already exists; skipping creation"
+  exit 0
+fi
+
+createdb --username "$POSTGRES_USER" "$MIRROR_DB"
+echo "Created mirror database '$MIRROR_DB'"
